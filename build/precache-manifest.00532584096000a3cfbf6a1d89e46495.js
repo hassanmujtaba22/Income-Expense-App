7c2fedcf8012aeb37e62bf02b065bb32c0b2e6df
hassanmujtaba <hassan03203406130@gmail.com>
2020-06-13 04:56:47 +0500

    build folder created for production

---
+++ build/precache-manifest.00532584096000a3cfbf6a1d89e46495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f526176f9022588dbc12f382cfd98c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7dc778b96406e4ac1a9",
+    "url": "/static/css/main.474d20d5.chunk.css"
+  },
+  {
+    "revision": "8412f8d4df3b1a4f91d1",
+    "url": "/static/js/2.08a78117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08a78117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7dc778b96406e4ac1a9",
+    "url": "/static/js/main.e9bfe425.chunk.js"
+  },
+  {
+    "revision": "061f0cbdc545e3c4ac6a",
+    "url": "/static/js/runtime-main.3b1a14b5.js"
+  }
+]);
